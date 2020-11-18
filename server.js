ea2fdc6cb25962aae515f227260638b0d4132c8b
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-11-18 18:34:26 -0500

    Added a view route accessing db.json file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //1. Require Express
 const express = require("express");
+const fs = require("fs");
+const { appendFile } = require("fs");
 const path = require("path");
 
 //2.Create Instance of Express
@@ -17,14 +19,22 @@ app.use(express.json());
 // app.get("/api/config", (req,res) =>{
 //     res.send("My config object will go here");
 // });
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+
 app.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-
+// Added a test note to see notes
+app.get("/api/notes", (req,res) => {
+    fs.readFile("./db/db.json",(err,data) => {
+        if (err) throw err;
+        const savedNotes = JSON.parse(data);
+        return res.json(savedNotes);
+    });
+});
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 //4. Listen on the port
 app.listen(PORT,() => {
     console.log(`App listening on http://localhost:${PORT}`);
